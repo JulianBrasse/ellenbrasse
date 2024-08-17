982769e67f82419c0028675d9dcc8129f1f6108b
Julian Brasse <julian.brasse@gmail.com>
2024-08-17 12:10:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function addheader() {
-	const header = document.querySelector("header");
-	const logo = document.createElement("a");
-		logo.classList.add("logo");
-		logo.href = "/";
-		header.appendChild(logo);
-}
 
-document.addEventListener("DOMContentLoaded", addheader);
